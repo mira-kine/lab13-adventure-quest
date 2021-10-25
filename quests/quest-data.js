e5ab07a9a19758b108c73910370fe052ee7414c1
mira-kine <mirak0419@gmail.com>
2021-10-25 14:06:25 -0700

    quest-data skeleton done

---
+++ quests/quest-data.js
@@ -0,0 +1,93 @@
+// consts Shibuya, Asakusa, Tokyo Fish Market, Kichijoji, Ueno
+// id, title, map location, image, description, choices, happy meter: , tired meter: , 
+
+const shibuya = {
+    id: 'shibuya',
+    title: 'Welcome to Shibuya',
+    // map: {},
+    // image: ,
+    // description: ,
+    // choices: [{
+        //id
+        //description
+        //result
+        //happy meter
+        //tired meter
+    //}]
+};
+
+const asakusa = {
+    id: 'Asakusa',
+    title: 'Welcome to Asakusa',
+    // map: {},
+    // image: ,
+    // description: ,
+    // choices: [{
+        //id
+        //description
+        //result
+        //happy meter
+        //tired meter
+    //}]
+};
+
+const tsukiji = {
+    id: 'tsukiji',
+    title: 'Welcome to Tsukiji',
+    // map: {},
+    // image: ,
+    // description: ,
+    // choices: [{
+        //id
+        //description
+        //result
+        //happy meter
+        //tired meter
+    //}]
+};
+
+const kichijoji = {
+    id: 'kichijoji',
+    title: 'Welcome to Kichijoji',
+    // map: {},
+    // image: ,
+    // description: ,
+    // choices: [{
+        //id
+        //description
+        //result
+        //happy meter
+        //tired meter
+    //}]
+};
+
+const ueno = {
+    id: 'ueno',
+    title: 'Welcome to Ueno',
+    // map: {},
+    // image: ,
+    // description: ,
+    // choices: [{
+        //id
+        //description
+        //result
+        //happy meter
+        //tired meter
+    //}]
+};
+
+const harajuku = {
+    id: 'harajuku',
+    title: 'Welcome to Harajuku',
+    // map: {},
+    // image: ,
+    // description: ,
+    // choices: [{
+        //id
+        //description
+        //result
+        //happy meter
+        //tired meter
+    //}]
+};
+
